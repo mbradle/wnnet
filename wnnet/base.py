b2b6ad4deb5f1d5b2ae1c6dd15905b74e491086f
Bradley Meyer <mbradle@clemson.edu>
2022-12-27 15:23:34 -0500

    Update.

--- wnnet/base.py
+++ wnnet/base.py
@@ -1,7 +1,7 @@
 import wnutils.xml as wx
 import numpy as np
 from astropy.constants import c, m_e
-
+from scipy.interpolate import interp1d
 
 class Base:
     """A class for computing and graphing flows."""
@@ -35,7 +35,7 @@ class Base:
 
         return result
 
-    def compute_duplicate_factor(self, elements):
+    def _compute_duplicate_factor(self, elements):
         dict = {}
         for sp in elements:
             if sp in dict:
@@ -49,9 +49,15 @@ class Base:
 
     def compute_reaction_duplicate_factors(self, reaction):
         return (
-            self.compute_duplicate_factor(reaction.nuclide_reactants),
-            self.compute_duplicate_factor(reaction.nuclide_products),
+            self._compute_duplicate_factor(reaction.nuclide_reactants),
+            self._compute_duplicate_factor(reaction.nuclide_products),
         )
+
+    def compute_duplicate_factors(self, reactions):
+        result = {}
+        for r in reactions:
+            result[r] = self.compute_reaction_duplicate_factors(reactions[r])
+        return result
 
     def is_weak_reaction(self, reaction):
         result = False
@@ -65,3 +71,34 @@ class Base:
             if sp not in nuclides:
                 return False
         return True
+
+    def compute_nuclear_partition_function(self, nuclide, t9):
+        "Method to compute the nuclear partition function for a species.
+
+        Args:
+            ``nuclide``: A `wnutils <https://wnutils.readthedocs.io>`_ nuclide.
+
+            ``t9`` (:obj:float): The temperature in 10<sup>9</sup> K at which
+            to compute the partition function.
+
+        Returns:
+            A :obj:float giving the nuclear partition function for the species
+            at the input temperature.
+
+        """
+
+        t = nuclide["t9"]
+        lg = np.log10(nuclide["partf"])
+
+        if t9 < t[0]:
+            return np.power(10.0, lg[0])
+        elif t9 > t[len(t) - 1]:
+            return np.power(10.0, lg[len(t) - 1])
+
+        if len(t) <= 2:
+            f = interp1d(t, lg, kind="linear")
+            return np.power(10.0, f(t9))
+        else:
+            f = interp1d(t, lg, kind="cubic")
+            return np.power(10.0, f(t9))
+
--- wnnet/flows.py
+++ wnnet/flows.py
@@ -3,95 +3,82 @@ import wnnet.base as fb
 import numpy as np
 
 
-class Flows(fb.Base):
-    """A class for computing and graphing flows."""
+def compute_flows_for_zones(net, zones, nuc_xpath="", reac_xpath=""):
+    """A class to compute flows for a set of zones.
 
-    def __init__(self, xml):
-        self.xml = xml
-        self.nuclides = xml.get_nuclide_data()
-        self.reactions = xml.get_reaction_data()
+    Args:
+        ``net``: A wnnet network.
 
-    def compute_flows_for_zones(self, zones, induced_nuc_xpath="", induced_reac_xpath=""):
-        """A class to compute flows for a set of zones.
+        ``zones`` (:obj:`dict`): A dictionary of `wnutils <https://wnutils.readthedocs.io>`_ *zone data*.
 
-        Args:
-            ``zones`` (:obj:`dict`): A dictionary of `wnutils <https://wnutils.readthedocs.io>`_ *zone data*.
+        ``nuc_xpath`` (:obj:`str`, optional): XPath expression
+        to select nuclides for flow computations.  Defaults to all
+        species.
 
-            ``induced_nuc_xpath`` (:obj:`str`, optional): XPath expression
-            to select nuclides for flow computations.  Defaults to all
-            species.
+        ``reac_xpath`` (:obj:`str`, optional): XPath expression
+        to select reactions for flow computations.  Defaults to all
+        reactions.
 
-            ``induced_reac_xpath`` (:obj:`str`, optional): XPath expression
-            to select reactions for flow computations.  Defaults to all
-            reactions.
+    Returns:
+        :obj:`dict`: A dictionary of flows for each zone.  The data for
+        each zone are themselvs a :obj:`dict` of reactions with each
+        item in the dictionary a tuple giving the forward and
+        reverse flow.
 
-        Returns:
-            :obj:`dict`: A dictionary of flows for each zone.  The data for
-            each zone are themselvs a :obj:`dict` of reactions with each
-            item in the dictionary a tuple giving the forward and
-            reverse flow.
+    """
 
-        """
+    nuclides = net.get_nuclides()
+    reactions = net.get_reactions()
 
-        subset_nuclides = {}
-        subset_reactions = {}
-        if induced_nuc_xpath is None:
-            subset_nuclides = self.nuclides
-        else:
-            subset_nuclides = self.xml.get_nuclide_data(nuc_xpath=induced_nuc_xpath)
-        if induced_reac_xpath is None:
-            subset_reactions = self.reactions
-        else:
-            subset_reactions = self.xml.get_reaction_data(reac_xpath=induced_reac_xpath)
+    dups = net.compute_duplicate_factors(reactions)
 
-        zone_flows = {}
+    zone_flows = {}
 
-        for zone in zones:
-            s_t9 = "t9"
-            s_rho = "rho"
-            props = zones[zone]["properties"]
-            x = zones[zone]["mass fractions"]
-            f = {}
-            if s_t9 in props and s_rho in props:
-                for reaction in self.get_valid_reactions(
-                    subset_nuclides, subset_reactions
-                ):
-                    my_reaction = subset_reactions[reaction]
-                    dup_f, dup_r = self.compute_reaction_duplicate_factors(my_reaction)
+    for zone in zones:
+        s_t9 = "t9"
+        s_rho = "rho"
+        props = zones[zone]["properties"]
+        x = zones[zone]["mass fractions"]
+        f = {}
+        if s_t9 in props and s_rho in props:
+            for reaction in net.get_valid_reactions(
+                nuc_xpath = nuc_xpath, reac_xpath = reac_xpath
+            ):
+                my_reaction = reactions[reaction]
 
-                    forward = my_reaction.compute_rate(float(props[s_t9]))
-                    forward *= np.power(
-                        float(props[s_rho]), len(my_reaction.nuclide_reactants) - 1
+                forward = my_reaction.compute_rate(float(props[s_t9]))
+                forward *= np.power(
+                    float(props[s_rho]), len(my_reaction.nuclide_reactants) - 1
+                )
+                forward *= dups[reaction][0]
+                for sp in my_reaction.nuclide_reactants:
+                    tup = net.xml.get_z_a_state_from_nuclide_name(sp)
+                    key = (sp, tup[0], tup[1])
+                    if key in x:
+                        y = x[key] / nuclides[sp]["a"]
+                    else:
+                        y = 0
+                    forward *= y
+
+                if not net.is_weak_reaction(my_reaction):
+                    reverse = my_reaction.compute_rate(float(props[s_t9]))
+                    reverse *= np.power(
+                        float(props[s_rho]), len(my_reaction.nuclide_products) - 1
                     )
-                    forward *= dup_f
-                    for sp in my_reaction.nuclide_reactants:
-                        tup = self.xml.get_z_a_state_from_nuclide_name(sp)
+                    reverse *= dups[reaction][1]
+                    for sp in my_reaction.nuclide_products:
+                        tup = net.xml.get_z_a_state_from_nuclide_name(sp)
                         key = (sp, tup[0], tup[1])
                         if key in x:
-                            y = x[key] / subset_nuclides[sp]["a"]
+                            y = x[key] / nuclides[sp]["a"]
                         else:
                             y = 0
-                        forward *= y
+                        reverse *= y
+                else:
+                    reverse = 0
 
-                    if not self.is_weak_reaction(my_reaction):
-                        reverse = my_reaction.compute_rate(float(props[s_t9]))
-                        reverse *= np.power(
-                            float(props[s_rho]), len(my_reaction.nuclide_products) - 1
-                        )
-                        reverse *= dup_r
-                        for sp in my_reaction.nuclide_products:
-                            tup = self.xml.get_z_a_state_from_nuclide_name(sp)
-                            key = (sp, tup[0], tup[1])
-                            if key in x:
-                                y = x[key] / subset_nuclides[sp]["a"]
-                            else:
-                                y = 0
-                            reverse *= y
-                    else:
-                        reverse = 0
+                f[reaction] = (forward, reverse)
 
-                    f[reaction] = (forward, reverse)
+        zone_flows[zone] = f
 
-            zone_flows[zone] = f
-
-        return zone_flows
+    return zone_flows
