96c473b3a65c6d9a32fab9d20df2d8e7e40257e6
Bradley Meyer <mbradle@clemson.edu>
2024-08-07 18:30:39 -0400

    Fix for pylint.

--- wnnet/nuc.py
+++ wnnet/nuc.py
@@ -2,8 +2,8 @@
 
 import wnutils.xml as wx
 import numpy as np
-import wnnet.consts as wc
 from scipy.interpolate import interp1d
+import wnnet.consts as wc
 
 
 class Nuc:
@@ -12,7 +12,8 @@ class Nuc:
     Args:
         ``file`` (:obj:`str`): A string giving the XML file name with the nuclide data.
 
-        ``nuc_xpath`` (:obj:`str`, optional): An XPath expression to select nuclides.  Default is all nuclides.
+        ``nuc_xpath`` (:obj:`str`, optional): An XPath expression to select
+        nuclides.  Default is all nuclides.
 
     """
 
@@ -25,7 +26,8 @@ class Nuc:
         """Method to return a collection of nuclides.
 
         Args:
-            ``nuc_xpath`` (:obj:`str`, optional): An XPath expression to select the nuclides.  Default is all species.
+            ``nuc_xpath`` (:obj:`str`, optional): An XPath expression to select
+             the nuclides.  Default is all species.
 
         Returns:
             A :obj:`dict` containing `wnutils <https://wnutils.readthedocs.io>`_ nuclides.
@@ -38,14 +40,14 @@ class Nuc:
             )
         return self.nuclides[nuc_xpath]
 
-    def compute_nuclear_partition_function(self, name, t9):
+    def compute_nuclear_partition_function(self, name, t_9):
         """Method to compute the nuclear partition function for a species.
 
         Args:
             ``name`` (:obj:`str`): A string giving the name of the nuclide
             whose partition function should be computed.
 
-            ``t9`` (:obj:`float`): The temperature in 10\ :sup:`9` K at which
+            ``t_9`` (:obj:`float`): The temperature in 10\\ :sup:`9` K at which
             to compute the partition function.
 
         Returns:
@@ -56,55 +58,60 @@ class Nuc:
 
         nuclide = self.get_nuclides()[name]
 
-        t = nuclide["t9"]
+        v_t = nuclide["t9"]
 
-        if len(t) == 0:
+        if len(v_t) == 0:
             return 2.0 * nuclide["spin"] + 1
 
-        lg = np.log10(nuclide["partf"])
+        l_g = np.log10(nuclide["partf"])
 
-        if t9 < t[0]:
-            return np.power(10.0, lg[0])
-        elif t9 > t[len(t) - 1]:
-            return np.power(10.0, lg[len(t) - 1])
+        if t_9 < v_t[0]:
+            return np.power(10.0, l_g[0])
 
-        if len(t) <= 2:
-            f = interp1d(t, lg, kind="linear")
-            return np.power(10.0, f(t9))
-        else:
-            f = interp1d(t, lg, kind="cubic")
-            return np.power(10.0, f(t9))
+        if t_9 > v_t[len(v_t) - 1]:
+            return np.power(10.0, l_g[len(v_t) - 1])
 
-    def compute_quantum_abundance(self, name, t9, rho):
-        """Method to compute the quantum abundance of the nuclide at the input temperature and density.
+        if len(v_t) <= 2:
+            f_int = interp1d(v_t, l_g, kind="linear")
+            return np.power(10.0, f_int(t_9))
+
+        f_int = interp1d(v_t, l_g, kind="cubic")
+        return np.power(10.0, f_int(t_9))
+
+    def compute_quantum_abundance(self, name, t_9, rho):
+        """Method to compute the quantum abundance of the nuclide at the
+        input temperature and density.
 
         Args:
             ``name`` (:obj:`str`): The name of the species.
 
-            ``t9`` (:obj:`float`): The temperature in 10\ :sup:`9` K at which to compute the quantum abundance.
+            ``t_9`` (:obj:`float`): The temperature in 10\\ :sup:`9` K at which
+             to compute the quantum abundance.
 
-            ``rho`` (:obj:`float`): The density in g/cc  at which to compute the quantum abundance.
+            ``rho`` (:obj:`float`): The density in g/cc  at which to compute
+            the quantum abundance.
 
         Returns:
-            A :obj:`float` giving the quantum abundance for the species at the input conditions.
+            A :obj:`float` giving the quantum abundance for the species
+            at the input conditions.
 
         """
 
-        assert t9 > 0 and rho > 0
-        assert t9 > 0 and rho > 0
+        assert t_9 > 0 and rho > 0
+        assert t_9 > 0 and rho > 0
 
         nuclide = self.get_nuclides()[name]
 
-        m = wc.m_u_in_MeV * nuclide["a"] + nuclide["mass excess"]
+        mass = wc.m_u_in_MeV * nuclide["a"] + nuclide["mass excess"]
 
-        result = self.compute_nuclear_partition_function(name, t9) / (
+        result = self.compute_nuclear_partition_function(name, t_9) / (
             rho * wc.N_A
         )
 
-        p1 = (m * wc.MeV_to_ergs) * wc.k_B * t9 * 1.0e9
-        p2 = 2.0 * np.pi * np.power(wc.hbar * wc.c, 2)
+        p_1 = (mass * wc.MeV_to_ergs) * wc.k_B * t_9 * 1.0e9
+        p_2 = 2.0 * np.pi * np.power(wc.hbar * wc.c, 2)
 
-        result *= np.power((p1 / p2), 1.5)
+        result *= np.power((p_1 / p_2), 1.5)
 
         return result
 
@@ -130,7 +137,7 @@ class Nuc:
             - nuclide["mass excess"]
         )
 
-    def _compute_NSE_factor(self, name, t9, rho):
-        return np.log(self.compute_quantum_abundance(name, t9, rho)) + (
+    def _compute_nse_factor(self, name, t_9, rho):
+        return np.log(self.compute_quantum_abundance(name, t_9, rho)) + (
             (self.compute_binding_energy(name) * wc.MeV_to_ergs)
-        ) / (wc.k_B * (t9 * 1.0e9))
+        ) / (wc.k_B * (t_9 * 1.0e9))
--- wnnet/zones.py
+++ wnnet/zones.py
@@ -3,30 +3,42 @@
 import wnutils.xml as wx
 
 
-class Zones_Xml:
+class Zones:
     """A class for handling webnucleo zones.
 
     Args:
-        ``file`` (:obj:`str`):  A string giving the XML file name with the zone data.
+        ``file`` (:obj:`str`):  A string giving the XML file name with the
+        zone data.
 
     """
 
-    def __init__(self, file):
-        self.xml = wx.Xml(file)
-        self.zones = self.xml.get_zone_data()
+    def __init__(self):
+        self.xml = None
+
+    def store_data_from_xml(self, xml_file):
+        """Method to retrieve zone data from an xml file.
+
+        Args:
+            ``xml_file`` (:obj:`str`):  The name of the XML file with zone
+            data.
+
+        Returns:
+            On successful return, the data have been stored.
+
+        """
+        self.xml = wx.Xml(xml_file)
 
     def get_zones(self, zone_xpath=""):
         """Method to return zones.
 
         Args:
-            ``zone_xpath`` (:obj:`str`, optional):  An XPath expression to select zones.  Default is all zones.
+            ``zone_xpath`` (:obj:`str`, optional):  An XPath expression to
+            select zones.  Default is all zones.
 
         Returns:
-            A :obj:`dict` of `wnutils <https://wnutils.readthedocs.io>`_ zone data objects.
+            A :obj:`dict` of `wnutils <https://wnutils.readthedocs.io>`_ zone
+            data objects.
 
         """
 
-        if not zone_xpath:
-            return self.zones
-        else:
-            return self.xml.get_zone_data(zone_xpath=zone_xpath)
+        return self.xml.get_zone_data(zone_xpath=zone_xpath)
