59bd12b3569fad0b65b29d111d77be6731a46a38
Bradley Meyer <mbradle@clemson.edu>
2024-08-10 18:05:48 -0400

    Fix consts.  Update lint test.

--- wnnet/consts.py
+++ wnnet/consts.py
@@ -4,30 +4,30 @@ also includes all constants from
 """
 
 import numpy as np
-from gslconsts.consts import *
+import gslconsts.consts as gc
 
 # Conversions for use with the package.
 
 #: Speed of light in vacuum (cm/s)
-c = GSL_CONST_CGSM_SPEED_OF_LIGHT
+c = gc.GSL_CONST_CGSM_SPEED_OF_LIGHT
 
 #: Boltzmann's constant (ergs/K)
-k_B = GSL_CONST_CGSM_BOLTZMANN
+k_B = gc.GSL_CONST_CGSM_BOLTZMANN
 
 #: Electron mass (g)
-m_e = GSL_CONST_CGSM_MASS_ELECTRON
+m_e = gc.GSL_CONST_CGSM_MASS_ELECTRON
 
 #: Amu mass (g)
-u = GSL_CONST_CGSM_UNIFIED_ATOMIC_MASS
+u = gc.GSL_CONST_CGSM_UNIFIED_ATOMIC_MASS
 
 #: Avogadro's number
-N_A = GSL_CONST_NUM_AVOGADRO
+N_A = gc.GSL_CONST_NUM_AVOGADRO
 
 #: Planck's constant divided by 2pi (ergs/s)
-hbar = GSL_CONST_CGSM_PLANCKS_CONSTANT_HBAR
+hbar = gc.GSL_CONST_CGSM_PLANCKS_CONSTANT_HBAR
 
 #: Conversion factor from MeVs to ergs
-MeV_to_ergs = 1.0e6 * GSL_CONST_CGSM_ELECTRON_VOLT
+MeV_to_ergs = 1.0e6 * gc.GSL_CONST_CGSM_ELECTRON_VOLT
 
 #: Conversion factor from ergs to MeVs
 ergs_to_MeV = 1.0 / MeV_to_ergs
