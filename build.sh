d38095c6ba31d9b4e9b4bd1dcb29a8463492ebcf
Bradley Meyer <mbradle@clemson.edu>
2024-08-22 09:29:19 -0400

    Add checks.

--- build.sh
+++ build.sh
@@ -1,6 +1,29 @@
 # Script to automate build for PyPI.
 
 rm -fr dist
-black --line-length=79 wnnet/*.py
+cd wnnet
+black --line-length=79 *.py
+pylint --disable=C0103 consts.py
+pylint __about__.py __init__.py flows.py  graph.py graph_helper.py net.py  nuc.py reac.py zones.py
+
+cd ../.github/workflows/
+pytest
+cd ../..
+
 python -m pip install --upgrade build
 python -m build
+python -m pip install --upgrade twine
+
+echo ""
+echo "All version numbers must be the same:"
+echo ""
+
+grep version wnnet/__about__.py | grep -v ","
+grep version CITATION.cff | grep -v "cff-version"
+grep Version doc/source/changelog.rst | grep -v Versioning | head -1
+
+echo ""
+echo "Check the release date:"
+echo ""
+grep date CITATION.cff
+echo ""
