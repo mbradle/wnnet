f0cff8dbcc27b4d88131cbb2db63234c97c1b6b6
Bradley Meyer <mbradle@clemson.edu>
2025-11-15 10:14:25 -0500

    Change to use classes.

--- wnnet/thermo.py
+++ wnnet/thermo.py
@@ -5,171 +5,380 @@ import wnstatmech as ws
 import wnnet.consts as wc
 
 
-def _compute_baryon_quantity(nuc, quantity, t_9, rho, mass_fractions):
-
-    result = 0
-
-    kt = wc.k_B * 1.0e9 * t_9
-
-    for key, value in mass_fractions.items():
-        y = value / key[2]
-
-        if y > 1.e100:
-
-            n_den = rho * wc.N_A * y
-
-            if quantity == "pressure":
-                result += n_den * kt
-            elif quantity == "energy density":
-                result += n_den * (
-                    nuc.compute_atomic_mass(key[0]) * wc.MeV_to_ergs
-                    + 1.5 * kt
-                )
-            elif quantity == "internal energy density":
-                result += n_den * 1.5 * kt
-            elif quantity == "entropy density":
-                result += (
-                    n_den
-                    * wc.k_B
-                    * (
-                        5.0 / 2.0
-                        - np.log(
-                            y / nuc.compute_quantum_abundance(key[0], t_9, rho)
-                        )
-                    )
-                )
-
-    return result
-
-
-def compute_thermo_quantity(nuc, quantity, t_9, rho, mass_fractions):
-    """A routine to compute a thermodynamic quantity for a given set of\
-    mass fractions at the input temperature and density.
+class Nuclei:
+    """A class for computing thermodynamic quantities for nuclei.
 
     Args:
         ``nuc``: A wnnet nuclide collection.
 
-        ``quantity`` (:obj:`str`):  The thermodynamic quantity to compute.
-
-        ``t_9`` (:obj:`float`):  The temperature in 10\\ :sup:`9` K at which\
-          to compute the flows.
-
-        ``rho`` (:obj:`float`):  The density in g/cc at which to compute the\
-          flows.
-
-        ``mass_fractions`` (:obj:`float`):\
-        A `wnutils <https://wnutils.readthedocs.io>`_ dictionary of mass\
-        fractions.
-
-    Returns:
-        A :obj:`dict` with keys *electron*, *photon*, *baryon*,
-        and *total* and the values being the computed quantity for
-        each key.
 
     """
 
-    assert quantity in (
-        "pressure",
-        "energy density",
-        "internal energy density",
-        "entropy density",
-    )
+    def __init__(self, nuc):
+        self.nuc = nuc
+        self.functions = {}
 
-    result = {}
-
-    temperature = 1.0e9 * t_9
-
-    # Electrons
-
-    y_e = 0
-    for key, value in mass_fractions.items():
-        y_e += key[1] * value / key[2]
-
-    n_e = rho * wc.N_A * y_e
-
-    electron = ws.fermion.create_electron()
-
-    alpha = electron.compute_chemical_potential(temperature, n_e)
-
-    result["electron"] = electron.compute_quantity(
-        quantity, temperature, alpha
-    )
-
-    # Photons
-
-    photon = ws.boson.create_photon()
-
-    result["photon"] = photon.compute_quantity(quantity, temperature, 0)
-
-    # Baryons
-
-    result["baryon"] = _compute_baryon_quantity(
-        nuc, quantity, t_9, rho, mass_fractions
-    )
-
-    # Total
-
-    total = 0
-    for value in result.values():
-        total += value
-
-    result["total"] = total
-
-    return result
-
-
-def compute_thermo_quantity_in_zone(nuc, quantity, zone):
-    """A routine to compute a thermodynamic quantity for the input zone.
-
-    Args:
-        ``nuc``: A wnnet nuclide collection.
-
-        ``quantity`` (:obj:`str`):  The thermodynamic quantity to compute.
-
-        ``zone`` (:obj:`dict`): A `wnutils <https://wnutils.readthedocs.io>`_ *zone*.
-
-    Returns:
-        A :obj:`dict` keys *electron*, *photon*, *baryon*,
-        and *total* and the values being the computed quantity for
-        the zone.
-
-    """
-
-    return compute_thermo_quantity(
-        nuc,
-        quantity,
-        float(zone["properties"]["t9"]),
-        float(zone["properties"]["rho"]),
-        zone["mass fractions"],
-    )
-
-
-def compute_thermo_quantity_for_zones(nuc, quantity, zones):
-    """A routine to compute a thermodynamic quantity for the input zone.
-
-    Args:
-        ``nuc``: A wnnet nuclide collection.
-
-        ``quantity`` (:obj:`str`):  The thermodynamic quantity to compute.
-
-        ``zones`` (:obj:`dict`): A dictionary of
-         `wnutils <https://wnutils.readthedocs.io>`_ *zone data*.
-
-    Returns:
-        A :obj:`dict` with the key being the zone label and the value
-        being another dictionary with keys *electron*, *photon*, *baryon*,
-        and *total* and the values being the computed quantity for
-        each zone.
-
-    """
-
-    result = {}
-
-    for key, value in zones.items():
-        result[key] = compute_thermo_quantity_in_zone(
-            nuc,
-            quantity,
-            value,
+        self.functions["number density"] = self.default_number_density
+        self.functions["pressure"] = self.default_pressure
+        self.functions["entropy density"] = self.default_entropy_density
+        self.functions["energy density"] = self.default_energy_density
+        self.functions["internal self.energy density"] = (
+            self.default_internal_energy_density
         )
 
-    return result
+    def _compute_abundances(self, mass_fractions):
+        y = {}
+        for key, value in mass_fractions.items():
+            y[key[0]] = value / key[2]
+        return y
+
+    def default_number_density(self, t9, rho, mass_fractions):
+        """The default routine for computing the number density of nuclei.
+
+        Args:
+            ``t9`` (:obj:`float`):  The temperature in 10\\ :sup:`9` K at which\
+            to compute the quantity.
+
+            ``rho`` (:obj:`float`):  The density in g/cc at which to compute the\
+            quantity.
+
+            ``mass_fractions`` (:obj:`float`):\
+                A `wnutils <https://wnutils.readthedocs.io>`_ dictionary of mass\
+                fractions.
+
+        Returns:
+            A :obj:`float` giving the number density in cgs units.
+
+        """
+        assert t9 > 0
+        y = self._compute_abundances(mass_fractions)
+
+        result = 0
+        for val in y.values():
+            result += rho * wc.N_A * val
+
+        return result
+
+    def default_pressure(self, t9, rho, mass_fractions):
+        """The default routine for computing the pressure of nuclei.
+
+        Args:
+            ``t9`` (:obj:`float`):  The temperature in 10\\ :sup:`9` K at which\
+            to compute the quantity.
+
+            ``rho`` (:obj:`float`):  The density in g/cc at which to compute the\
+            quantity.
+
+            ``mass_fractions`` (:obj:`float`):\
+                A `wnutils <https://wnutils.readthedocs.io>`_ dictionary of mass\
+                fractions.
+
+        Returns:
+            A :obj:`float` giving the pressure in cgs units.
+
+        """
+        n_den = self.default_number_density(t9, rho, mass_fractions)
+        return wc.k_B * 1.0e9 * t9 * n_den
+
+    def default_entropy_density(self, t9, rho, mass_fractions):
+        """The default routine for computing the entropy density of nuclei.
+
+        Args:
+            ``t9`` (:obj:`float`):  The temperature in 10\\ :sup:`9` K at which\
+            to compute the quantity.
+
+            ``rho`` (:obj:`float`):  The density in g/cc at which to compute the\
+            quantity.
+
+            ``mass_fractions`` (:obj:`float`):\
+                A `wnutils <https://wnutils.readthedocs.io>`_ dictionary of mass\
+                fractions.
+
+        Returns:
+            A :obj:`float` giving the entropy density in cgs units.
+
+        """
+        y = self._compute_abundances(mass_fractions)
+
+        result = 0
+        for key, value in y.items():
+            result += (
+                value
+                * rho
+                * wc.N_A
+                * wc.k_B
+                * (
+                    5.0 / 2.0
+                    - np.log(
+                        value
+                        / self.nuc.compute_quantum_abundance(key, t9, rho)
+                    )
+                )
+            )
+
+        return result
+
+    def default_energy_density(self, t9, rho, mass_fractions):
+        """The default routine for computing the energy density of nuclei,
+        which includes the rest-mass energy.
+
+        Args:
+            ``t9`` (:obj:`float`):  The temperature in 10\\ :sup:`9` K at which\
+            to compute the quantity.
+
+            ``rho`` (:obj:`float`):  The density in g/cc at which to compute the\
+            quantity.
+
+            ``mass_fractions`` (:obj:`float`):\
+                A `wnutils <https://wnutils.readthedocs.io>`_ dictionary of mass\
+                fractions.
+
+        Returns:
+            A :obj:`float` giving the energy density in cgs units.
+
+        """
+        y = self._compute_abundances(mass_fractions)
+
+        result = 0
+
+        kt = wc.k_B * 1.0e9 * t9
+
+        for key, value in y.items():
+            result += (
+                rho
+                * wc.N_A
+                * value
+                * (
+                    self.nuc.compute_atomic_mass(key) * wc.MeV_to_ergs
+                    + 1.5 * kt
+                )
+            )
+
+    def default_internal_energy_density(self, t9, rho, mass_fractions):
+        """The default routine for computing the energy density of nuclei,
+        which does not include the rest-mass energy.
+
+        Args:
+            ``t9`` (:obj:`float`):  The temperature in 10\\ :sup:`9` K at which\
+            to compute the quantity.
+
+            ``rho`` (:obj:`float`):  The density in g/cc at which to compute the\
+            quantity.
+
+            ``mass_fractions`` (:obj:`float`):\
+                A `wnutils <https://wnutils.readthedocs.io>`_ dictionary of mass\
+                fractions.
+
+        Returns:
+            A :obj:`float` giving the internal energy density in cgs units.
+
+        """
+        n_den = self.default_number_density(t9, rho, mass_fractions)
+        return 1.5 * wc.k_B * 1.0e9 * t9 * n_den
+
+    def compute_thermo_quantity(self, quantity, t9, rho, mass_fractions):
+        """Routine to compute the thermodynamic quantity for the nuclei.
+
+        Args:
+            ``quantity`` (:obj:`str`):  The thermodynamic quantity to compute.
+
+            ``t9`` (:obj:`float`):  The temperature in 10\\ :sup:`9` K at which\
+            to compute the quantity.
+
+            ``rho`` (:obj:`float`):  The density in g/cc at which to compute the\
+            quantity.
+
+            ``mass_fractions`` (:obj:`float`):\
+                A `wnutils <https://wnutils.readthedocs.io>`_ dictionary of mass\
+                fractions.
+
+        Returns:
+            A :obj:`float` giving the thermo quantity in cgs units.
+
+        """
+        assert quantity in self.functions
+
+        return self.functions[quantity](t9, rho, mass_fractions)
+
+    def update_thermo_function(self, quantity, function):
+        """Routine to update or add a thermodynamic function for the nuclei.
+
+        Args:
+            ``quantity`` (:obj:`str`):  The thermodynamic quantity.
+
+            ``function``: A function with prototype *(t9, rho, mass_fractions)*
+            to compute the thermodynamic quantity for the nuclei.
+
+        Returns:
+            On successful return, the function for the quantity has been
+            updated.
+
+        """
+        self.functions[quantity] = function
+
+
+class Thermo:
+    """A class for computing network thermodynamic quantities.
+
+    Args:
+        ``nuc``: A wnnet nuclide collection.
+
+
+    """
+
+    def __init__(self, nuc):
+        self.nuc = nuc
+        self.fermions = {}
+        self.bosons = {}
+        self.fermions["electron"] = ws.fermion.create_electron()
+        self.bosons["photon"] = ws.boson.create_photon()
+        self.nuclei = Nuclei(nuc)
+        self.number_density = {}
+
+    def set_number_density(self, particle, number_density):
+        """Routine to update the number density for a particle.
+
+        Args:
+            ``quantity`` (:obj:`str`):  The thermodynamic quantity.
+
+            ``number_density`` (:obj:`float`): The number density in
+            cgs units.
+
+        Returns:
+            On successful return, the number density has been updated.
+
+        """
+        self.number_density[particle] = number_density
+
+    def get_number_density(self, particle):
+        """Routine to retrieve the number density for a particle.
+
+        Args:
+            ``particle`` (:obj:`str`):  The name of the particle.
+
+        Returns:
+            On successful return, the number density has been updated.
+
+        """
+        return self.number_density[particle]
+
+    def compute_thermo_quantity(self, quantity, t9, rho, mass_fractions):
+        """A routine to compute a thermodynamic quantity for a given set of\
+        mass fractions at the input temperature and density.
+
+        Args:
+            ``quantity`` (:obj:`str`):  The thermodynamic quantity to compute.
+
+            ``t9`` (:obj:`float`):  The temperature in 10\\ :sup:`9` K at which\
+            to compute the quantity.
+
+            ``rho`` (:obj:`float`):  The density in g/cc at which to compute the\
+            quantity.
+
+            ``mass_fractions`` (:obj:`float`):\
+                A `wnutils <https://wnutils.readthedocs.io>`_ dictionary of mass\
+                fractions.
+
+        Returns:
+            A :obj:`dict` with keys *electron*, *photon*, *baryon*,
+            and *total* and the values being the computed quantity for
+            each key.
+
+        """
+
+        result = {}
+
+        temperature = 1.0e9 * t9
+
+        # Baryons
+
+        result["baryon"] = self.nuclei.compute_thermo_quantity(
+            quantity, t9, rho, mass_fractions
+        )
+
+        # Fermions
+
+        for name, fermion in self.fermions.items():
+            if name == "electron":
+                _n = 0
+                for key, value in mass_fractions.items():
+                    _n += rho * wc.N_A * key[1] * value / key[2]
+            else:
+                _n = self.get_number_density(fermion)
+
+            alpha = fermion.compute_chemical_potential(temperature, _n)
+
+            result[name] = fermion.compute_quantity(
+                quantity, temperature, alpha
+            )
+
+        # Bosons
+
+        for name, boson in self.bosons.items():
+            if name == "photon":
+                alpha = 0
+            else:
+                alpha = boson.compute_chemical_potential(
+                    temperature, self.get_number_density(boson)
+                )
+
+            result[name] = boson.compute_quantity(quantity, temperature, alpha)
+
+        # Total
+
+        total = 0
+        for value in result.values():
+            total += value
+
+        result["total"] = total
+
+        return result
+
+    def compute_thermo_quantity_in_zone(self, quantity, zone):
+        """A routine to compute a thermodynamic quantity for the input zone.
+
+        Args:
+           ``quantity`` (:obj:`str`):  The thermodynamic quantity to compute.
+
+           ``zone`` (:obj:`dict`): A `wnutils <https://wnutils.readthedocs.io>`_ *zone*.
+
+        Returns:
+            A :obj:`dict` keys *electron*, *photon*, *baryon*,
+            and *total* and the values being the computed quantity for
+            the zone.
+
+        """
+
+        return self.compute_thermo_quantity(
+            quantity,
+            float(zone["properties"]["t9"]),
+            float(zone["properties"]["rho"]),
+            zone["mass fractions"],
+        )
+
+    def compute_thermo_quantity_for_zones(self, quantity, zones):
+        """A routine to compute a thermodynamic quantity for the input zones.
+
+        Args:
+            ``quantity`` (:obj:`str`):  The thermodynamic quantity to compute.
+
+            ``zones`` (:obj:`dict`): A dictionary of
+            `wnutils <https://wnutils.readthedocs.io>`_ *zone data*.
+
+        Returns:
+            A :obj:`dict` with the key being the zone label and the value
+            being another dictionary with keys *electron*, *photon*, *baryon*,
+            and *total* and the values being the computed quantity for
+            each zone.
+
+        """
+
+        result = {}
+
+        for key, value in zones.items():
+            result[key] = self.compute_thermo_quantity_in_zone(
+                quantity,
+                value,
+            )
+
+        return result
